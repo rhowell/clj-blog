942ef3b245b5590eb5822ef4160aead291ffbf52
Ronnie Howell <ronniemhowell@gmail.com>
2014-06-21 15:29:29 -0400

    Added in http-kit

--- project.clj
+++ project.clj
@@ -1,10 +1,14 @@
 (defproject clj-blog "0.1.0-SNAPSHOT"
   :description "Clojure Blog Platform"
   :url "https://github.com/rhowell/clj-blog"
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [compojure "1.1.6"]]
-  :plugins [[lein-ring "0.8.10"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [compojure "1.1.6"]
+                 [ring/ring-devel "1.3.0"]
+                 [ring/ring-core "1.3.0"]
+                 [http-kit "2.1.16"]]
+  :plugins [[lein-ring "0.8.11"]]
   :ring {:handler clj-blog.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring-mock "0.1.5"]]}})
+                        [ring-mock "0.1.5"]]}}
+  :main clj-blog.handler)
--- src/clj_blog/handler.clj
+++ src/clj_blog/handler.clj
@@ -1,12 +1,19 @@
 (ns clj-blog.handler
-  (:use compojure.core)
-  (:require [compojure.handler :as handler]
+  (:use compojure.core
+        [compojure.handler :only [site]]
+        [org.httpkit.server :only [run-server]])
+  (:require [ring.middleware.reload :as reload]
             [compojure.route :as route]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] "Hello People")
   (route/resources "/")
   (route/not-found "Not Found"))
 
-(def app
-  (handler/site app-routes))
+(defn in-dev? [& args] true) ;; TODO read a config variable from command line, env, or file?
+
+(defn -main [& args] ;; entry point, lein run will pick up and start from here
+  (let [handler (if (in-dev? args)
+                  (reload/wrap-reload (site #'app-routes)) ;; only reload when dev
+                  (site app-routes))]
+    (run-server handler {:port 8080})))
